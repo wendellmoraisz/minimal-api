e1087b153018cc0595c493d8cc78e98bc8df3bd6
Wendell <wendellmorais.dev@gmail.com>
2024-10-01 20:03:41 -0300

    refactor: add regions on program.cs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ using minimal_api.Domain.ModelViews;
 using minimal_api.Domain.Services;
 using minimal_api.Infrastructure.Database;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdminService, AdminService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,18 +24,25 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
+# region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdminService adminService) =>
+#region Admin
+app.MapPost("/admin/login", ([FromBody] LoginDTO loginDTO, IAdminService adminService) =>
 {
     if (adminService.Login(loginDTO) is not null)
         return Results.Ok("Login com sucesso");
 
     return Results.Unauthorized();
 });
+#endregion
 
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
